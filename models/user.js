0cd0b3c34e06d8c0875a856ff67c7bbf894d7f27
anushamaniar <128983804+anushamaniar@users.noreply.github.com>
2024-05-02 15:56:39 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -2,13 +2,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
 
-const UserSchema = new Schema ({
+// Define the User schema
+const UserSchema = new Schema({
     email: {
-        type: String,
-        required: true,
-        unique: true
+        type: String, 
+        required: true, 
+        unique: true 
     }
 });
+
+// Apply the passportLocalMongoose plugin to the UserSchema
+// This plugin will add a username, hash and salt field to store the username, the hashed password and the salt value
 UserSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('User', UserSchema);

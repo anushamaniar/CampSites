01c6b2d37741276e83243cdff80988f0a44b2bb1
anushamaniar <128983804+anushamaniar@users.noreply.github.com>
2024-05-02 15:44:50 +0530

    Update campgrounds.js

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -5,48 +5,52 @@ const geocoder = mbxGeocoding({accessToken:mapboxToken});
 
 const {cloudinary} = require("../cloudinary");
 
+// Controller for handling GET request to list all campgrounds
 module.exports.index = async (req,res) => {
 
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', {campgrounds})
 }
 
+// Controller for handling GET request to show the new campground form
 module.exports.renderNewForm = (req,res) => {
     res.render('campgrounds/new');
 }
 
-module.exports.createCampground = async (req,res, next) => {
+// Controller for handling POST request to create a new campground
+module.exports.createCampground = async (req, res, next) => {
     const geoData = await geocoder.forwardGeocode({
-        query:req.body.campground.location,
-        limit:1
-    }).send()
-    //if(!req.body.Campground) throw new ExpressError("Invalid Campground Data",400);
+        query: req.body.campground.location, // Location to geocode
+        limit: 1 // Return only the best result
+    }).send();
     
     const campground = new Campground(req.body.campground);
-    campground.geometry = geoData.body.features[0].geometry;
-    campground.images = req.files.map(f=>({url:f.path, filename:f.filename}));
-    campground.author = req.user._id;
+    campground.geometry = geoData.body.features[0].geometry; // Set the geometry for the campground
+    campground.images = req.files.map(f => ({url: f.path, filename: f.filename})); // Map uploaded files to images
+    campground.author = req.user._id; // Set the author of the campground to the current user
     await campground.save();
     console.log(campground);
-    req.flash('success','Successfully created a new campground!');
-    res.redirect(`/campgrounds/${campground._id}`)
+    req.flash('success', 'Successfully created a new campground!');
+    res.redirect(`/campgrounds/${campground._id}`);
 }
 
-module.exports.showCampground = async (req,res) => {
+
+// Controller for handling GET request to show a single campground
+module.exports.showCampground = async (req, res) => {
     const campground = await Campground.findById(req.params.id).populate({
-        path:'reviews',
-        populate:{
-            path:'author'
+        path: 'reviews', // Populate the reviews of the campground
+        populate: {
+            path: 'author' // Populate the author of each review
         }
-    }).populate('author');
-    //console.log(campground);
-    if(!campground) {
+    }).populate('author'); // Populate the author of the campground
+    if (!campground) {
         req.flash('error', 'Cannot find that campground');
         return res.redirect('/campgrounds');
     }
-    res.render('campgrounds/show',{campground});
+    res.render('campgrounds/show', {campground});
 }
 
+// Controller for handling GET request to edit campground form
 module.exports.renderEditForm = async (req,res) => {
     const campground = await Campground.findById(req.params.id);
     if(!campground) {
@@ -56,6 +60,7 @@ module.exports.renderEditForm = async (req,res) => {
     res.render('campgrounds/edit',{campground});
 }
 
+// Controller for handling PUT request to update a campground
 module.exports.updateCampground =  async (req,res) => {
     const {id} = req.params;
     console.log(req.body)
@@ -74,8 +79,8 @@ module.exports.updateCampground =  async (req,res) => {
     res.redirect(`/campgrounds/${campground._id}`)
 }
 
+// Controller for handling DELETE request to delete a campground
 module.exports.deleteCampground = async (req,res) => {
-    
     await Campground.findByIdAndDelete(req.params.id);
     req.flash('success','Successfully deleted campground!');
     res.redirect(`/campgrounds`)

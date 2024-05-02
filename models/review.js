5a5bb5a832273b73853fe0978b2f1ce7e4ad652a
anushamaniar <128983804+anushamaniar@users.noreply.github.com>
2024-05-02 15:55:17 +0530

    Update review.js

--- models/review.js
+++ models/review.js
@@ -1,15 +1,14 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const reviewSchema = new Schema ({
-    body: String,
-    rating: Number,
+// Define the Review schema
+const reviewSchema = new Schema({
+    body: String, 
+    rating: Number, 
     author: {
-        type: Schema.Types.ObjectId,
-        ref:'User'
+        type: Schema.Types.ObjectId, // Reference to a user's ObjectId
+        ref: 'User' // Defines the reference to the User model, linking reviews to users
     }
 });
 
-
-
-module.exports = mongoose.model('Review',reviewSchema)
+module.exports = mongoose.model('Review', reviewSchema);
